d25c978d3b0af3a56c899438a657c37f387638df
Jennie Robinson Faber <jennie@cipritine.local>
2018-05-09 10:08:13 -0400

    Add some event stuff

--- app.js
+++ app.js
@@ -7,15 +7,30 @@ const sugarss          = require('sugarss')
 const cssStandards     = require('spike-css-standards')
 const jsStandards      = require('babel-preset-latest')
 const pageId           = require('spike-page-id')
+const df                      = require('dateformat')
+
 const SpikeDatoCMS            = require('spike-datocms')
 const postcssMixins           = require('postcss-mixins')
 const locals           = { }
 
+
+var offset = -4;
 const Dato = new SpikeDatoCMS({
   addDataTo: locals,
   token: process.env.dato_api_key,
   models: [{
-    name: 'event'
+    name: 'event',
+    transform: (data) => {
+      if (data.startDateTime) {
+        startDateHours = new Date(new Date(data.startDateTime).getTime() + offset * 3600 * 1000).toUTCString()
+        data.startDateTime = startDateHours
+      }
+      if (data.endDateTime) {
+        endDateHours = new Date( new Date(data.endDateTime).getTime() + offset * 3600 * 1000).toUTCString()
+        data.endDateTime = endDateHours
+      }
+      return data
+    }
   }]
 })
 
@@ -27,6 +42,7 @@ module.exports = {
     parser: sugarml,
     locals: (ctx) => { return Object.assign(locals,
       { pageId: pageId(ctx) },
+      { df: df.bind(df) }
     )},
     retext: { quotes: false }
   }),
