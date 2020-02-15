33dc5c2a62d61d124c8dfd24b3cb06bce5327ffa
Jennie Faber <jennie@tomediaarts.org>
2020-02-15 18:14:59 -0500

    Fix margin

--- src/templates/Signage-events.vue
+++ src/templates/Signage-events.vue
@@ -16,12 +16,12 @@
               "
             />
             <div class="absolute z-20 bottom-0">
-              <h3 class="text-6xl m-2 -mb-1 bg-white p-3">
-                {{ event.node.title }}
-              </h3>
-              <p class="text-4xl font-body font-normal inline m-2 bg-white p-3">
+              <p class="text-4xl font-body font-normal inline bg-white p-3">
                 {{ event.node.startDateTime | luxon:format('MMMM d') }}
               </p>
+              <h3 class="text-6xl mt-0 bg-white p-3">
+                {{ event.node.title }}
+              </h3>
             </div>
           </div>
         </TinySlider>
